3a3bb1e0ac9edaa566f35be4ab4bb99c473b5983
kochq <matukoch@live.com>
2023-12-08 18:47:14 -0300

    good identation for php

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -19,4 +19,8 @@ require 'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  -- Needed bucause treesitter highlight doesn't work with automatically with php
+  indent = {
+    enable = true,
+  },
 }
