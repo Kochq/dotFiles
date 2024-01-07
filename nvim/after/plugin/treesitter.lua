d1d5287a5fbc553649407b354e6010e6fdcb65cd
kochq <matukoch@live.com>
2024-01-07 15:33:45 -0300

    Disable treesitter on 4k files

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -13,6 +13,10 @@ require 'nvim-treesitter.configs'.setup {
         -- `false` will disable the whole extension
         enable = true,
 
+        disable = function(lang, bufnr) -- Disable in files with more than 4K
+            return vim.api.nvim_buf_line_count(bufnr) > 4000
+        end,
+
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
