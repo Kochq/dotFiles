652661b4ec388a45ea086ccab1ebaa22d8847da3
kochq <matukoch@live.com>
2024-09-29 18:42:30 -0300

    Debuggin | Nvim

---
+++ nvim/lua/plugins/masonDap.lua
@@ -0,0 +1,10 @@
+return {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+        "williamboman/mason.nvim",
+    },
+    opts = {
+        handlers = {},
+    },
+}
