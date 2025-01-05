c956e18ba21729052b1fdd1a5eb42bb14e4bb027
kochq <matukoch@live.com>
2025-01-05 18:46:20 -0300

    Nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -45,9 +45,9 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
+                "emmet-ls",
+                "emmet-lenguaje-server",
+                "eslint-lsp",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
