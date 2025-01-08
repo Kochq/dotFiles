9cad58ecabcbc0939e9d8f447e01fefe00e9b87e
kochq <matukoch@live.com>
2025-01-08 15:01:48 -0300

    Removing lsp-zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
+    "neovim/nvim-lspconfig",
     dependencies = {
-        "neovim/nvim-lspconfig",
+        "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
@@ -15,24 +15,10 @@ return {
     },
 
     config = function()
-        local lspZero = require("lsp-zero")
-        lspZero.on_attach(function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "]n", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]p", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
-        require 'lspconfig'.jdtls.setup {
-            root_dir = require('lspconfig/util').root_pattern('.git', 'pom.xml', 'build.gradle', 'src', 'ipoo'),
-        }
+        require("conform").setup({
+            formatters_by_ft = {
+            }
+        })
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -49,6 +35,7 @@ return {
                 "svelte",
                 "ts_ls",
                 "eslint",
+                "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -151,11 +138,11 @@ return {
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
                 ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
                 ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+                ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "path" },
-                { name = "luasnip" },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
             }, {
                 { name = 'buffer' },
             })
