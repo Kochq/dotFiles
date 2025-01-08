712707157931597607f5068f9b6a11367d36e6da
kochq <matukoch@live.com>
2025-01-08 15:19:43 -0300

    Fugitive | nvim

---
+++ nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,21 @@
+return {
+    "tpope/vim-fugitive",
+    config = function()
+
+        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = ThePrimeagen_Fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+            end,
+        })
+
+        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+    end
+}
