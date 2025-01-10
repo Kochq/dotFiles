6b4c9268a7f51ce8580c733d3c956d218567546f
kochq <matukoch@live.com>
2025-01-10 16:56:54 -0300

    Adding telescope ui

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     tag = "0.1.5",
 
     dependencies = {
+        "nvim-telescope/telescope-ui-select.nvim",
         "nvim-lua/plenary.nvim"
     },
 
@@ -18,6 +19,9 @@ return {
             }
         })
 
+        require("telescope").load_extension("ui-select")
+
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
         vim.keymap.set('n', '<leader>gs', builtin.git_files, {})
@@ -26,11 +30,11 @@ return {
         vim.keymap.set('n', '<leader>+', builtin.spell_suggest, {})
         vim.keymap.set('n', '<leader>}', builtin.colorscheme, {})
 
-        vim.keymap.set('n', '<leader>pws', function ()
+        vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>pWs', function ()
+        vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
