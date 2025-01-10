80c1e665f669caccd0e06d4dc16d8363ab56a424
kochq <matukoch@live.com>
2025-01-10 14:51:14 -0300

    Couple fixes :)

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,8 +1,6 @@
 return {
     "norcalli/nvim-colorizer.lua",
     config = function()
-        require("colorizer").setup({"*"})
-
         vim.keymap.set("n", "<leader>#","<cmd>ColorizerToggle<cr>")
     end,
 }
