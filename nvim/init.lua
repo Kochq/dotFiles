605572e414fb6ae80c9368f0d8888ab7b943a729
kochq <matukoch@live.com>
2025-03-22 19:57:20 -0300

    feat: Colorschemes

--- nvim/init.lua
+++ nvim/init.lua
@@ -797,13 +797,20 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'joshdick/onedark.vim',
+    'tomasr/molokai',
+    'shaunsingh/nord.nvim',
+    'catppuccin/nvim',
+    'rebelot/kanagawa.nvim',
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
